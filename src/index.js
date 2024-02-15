9ba8e5eb064f7ad5df0ec29783c3a1f20e99bdad
Tarun Parmar <tarunparmar752@gmail.com>
2024-02-15 13:09:05 +0530

    Feature: Adding Time Stamps

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
 
+const { generateMessage } = require("./utils/messages");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -18,33 +19,31 @@ let count = 0;
 
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
-  socket.emit("message", " Welcome!!");
-  socket.broadcast.emit('message',"A new user joined!")
+  socket.emit("message", generateMessage("Welcome!"));
+  socket.broadcast.emit("message", "A new user joined!");
 
-  socket.on("sendMessage", (message , callback) => {
+  socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed!");
     }
-    io.emit("message", message);
-    callback()
+    io.emit("message", generateMessage(message));
+    callback();
   });
 
-socket.on("sendLocation", (coordinates , callback) => {
-  io.emit(
-    "locationMessage",
-    `https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`
-  );
+  socket.on("sendLocation", (coordinates, callback) => {
+    io.emit(
+      "locationMessage",
+      `https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`
+    );
     callback();
-});
+  });
 
-    socket.on("disconnect", () => {
-      io.emit("message", "A user has left!");
-    });
+  socket.on("disconnect", () => {
+    io.emit("message", generateMessage("A user has left!"));
+  });
 });
-
- 
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
