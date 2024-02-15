be5e11dcdb431bf93299ec8140b3be02ba5aa313
Tarun Parmar <tarunparmar752@gmail.com>
2024-02-15 10:58:42 +0530

    Socket.io feature : Sending messages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+  socket.emit("message", " Welcome!!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
